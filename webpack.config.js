e06f161858c4e71543d57f3fa5db82d30f313a8a
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-04-05 01:54:41 -0300

    Configure webpack to deal with images extensions

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,13 @@ module.exports = {
         test: /\.scss$/,
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|svg)$/,
+        loader: 'file-loader',
+        options: {
+            name: 'assets/[name].[ext]',
+        }       
       }
     ],
   }
